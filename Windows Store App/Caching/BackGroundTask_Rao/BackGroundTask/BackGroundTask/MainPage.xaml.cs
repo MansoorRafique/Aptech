e03bbddb9025eac3478650ea9869c4ed2fbcbeee
MansoorRafique <mansoorrafiq158@outlook.com>
2017-05-11 11:10:03 +0500

    Add files via upload

---
+++ Windows Store App/Caching/BackGroundTask_Rao/BackGroundTask/BackGroundTask/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BackGroundTask
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //Unregistered tasks
+
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                task.Value.Unregister(true);
+            }
+
+
+            //task to be registered here...
+
+            //BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
+            var builder = new BackgroundTaskBuilder();
+            builder.Name = "MySampleTask";
+            builder.TaskEntryPoint = "BackgroundTasks.MyBackgroundTask"; //Namespace Class.name
+
+            //Task Trigger
+            builder.SetTrigger(new SystemTrigger(SystemTriggerType.NetworkStateChange, false));
+            var register = builder.Register();
+
+        
+        }
+    }
+}
---
+++ Windows Store App/Caching/BackGroundTask_Rao/BackGroundTask/BackgroundTasks/MyBackgroundTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace BackgroundTasks
+{
+    public sealed class MyBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        { 
+            //Background task Logic Building//
+            UpdateNetworkStatusAndTime();
+        }
+
+        private static void UpdateNetworkStatusAndTime()
+        {
+            var tileContent = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquareText02);
+            
+            tileContent.GetElementsByTagName("text")[0].InnerText = "ABC";
+            tileContent.GetElementsByTagName("text")[1].InnerText = DateTime.Now.ToString("MM/dd/yyyy");
+            tileContent.GetElementsByTagName("text")[2].InnerText = DateTime.Now.ToString("HH/mm/ss");
+
+            var notification = new TileNotification(tileContent);
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.Update(notification);
+        }
+    }
+}
